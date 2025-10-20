71ad16db455951e1c2f8f9e059f7cf23fa27e788
Ubaid-Stack <ubaidullahfareed2003@gmail.com>
2025-10-20 20:53:19 +0530

    Refactor code structure for improved readability and maintainability

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
   <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
     <link href="https://cdn.jsdelivr.net/npm/boxicons@2.1.4/css/boxicons.min.css" rel="stylesheet">
-  <link rel="stylesheet" href="style.css?v=20251019">
+  <link rel="stylesheet" href="style.css?v=20251020">
     <title>Home</title>
   </head>
   <body>
@@ -28,6 +28,8 @@
             </ul>
              <div class="nav-actions">
               <div class="menu"><i class='bx bx-search' id="searchIcon"></i></div>
+              <div class="menu"><i class='bx bx-cart' id="myCart"></i></div>
+              <div class="menu"><i class='bx bx-heart' id="wishlist"></i></div>
               <div class="menu"><i class='bx bx-menu'></i></div>
             </div>
             <!-- this is for search bar -->
@@ -37,6 +39,60 @@
             </div>
         </nav>
       </header>
+      <main class="body-con">
+        <section class="hero-section">
+          <div class="hero-content">
+            <h1 class="main-text">Unleash Your Style with Velvet Vogue</h1>
+            <p class="sub-text">Trendy, youthful, and bold — explore outfits that make every moment shine.</p>
+            <a href="#" class="shop-btn">Shop Now</a>
+            <a href="#" class="cat-btn">Explore Categories <i class='bx bx-chevron-right'></i></a>
+          </div>
+          <figure class="hero-image-container">
+            <img src="heroImg.png" alt="Hero Image" class="hero-image">
+          </figure>
+        </section>
+        <section class="Categories-section">
+          <h2 class="section-title">Shop by Category</h2>
+          <div class="categories-container ">
+            <div class="category-card">
+              <img src="./images/mensWear.webp" alt="Men's Wear" class="category-image">
+              <div class="navBtn">
+                <h3 class="category-title">Men's Wear</h3>
+                <button class="exploreBtn">Explore <i class='bx bx-chevron-right'></i></button>
+              </div>
+            </div>
+            <div class="category-card">
+              <img src="./images/womensWear.webp" alt="Women's Wear" class="category-image">
+              <div class="navBtn">
+                <h3 class="category-title">Women's Wear</h3>
+                <button class="exploreBtn">Explore <i class='bx bx-chevron-right'></i></button>
+              </div>
+            </div>
+            <div class="category-card">
+              <img src="./images/formal.jpg" alt="Formal Wear" class="category-image">
+              <div class="navBtn">
+                <h3 class="category-title">Formal Wear</h3>
+                <button class="exploreBtn">Explore <i class='bx bx-chevron-right'></i></button>
+              </div>
+            </div>
+            <div class="category-card">
+              <img src="./images/casual.webp" alt="Casual Wear" class="category-image">
+              <div class="navBtn">
+                <h3 class="category-title">Casual Wear</h3>
+                <button class="exploreBtn">Explore <i class='bx bx-chevron-right'></i></button>
+              </div>
+            </div>
+            <div class="category-card">
+              <img src="./images/accs.webp" alt="Accessories" class="category-image">
+              <div class="navBtn">
+                <h3 class="category-title">Accessories</h3>
+                <button class="exploreBtn">Explore <i class='bx bx-chevron-right'></i></button>
+              </div>
+            </div>
+          </div>
+        </section>
+        
+      </main>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
     <script>
       // this is search bar visible script
